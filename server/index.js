59efbdae8d21dc348414d7b16de7bd784bb7bcb2
Omkar Paschapur <your.email@example.com>
2024-08-10 11:48:47 +0530

    server setup and authentication

--- server/index.js
+++ server/index.js
@@ -8,20 +8,21 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { register } from './controllers/auth.js';
 
 /* Configurations */
 
-const __filename = fileURLToPaths560(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({ policy: "cros-origin"}));
+app.use(helmet.crossOriginEmbedderPolicy({ policy: "require-corp" })); 
 app.use(morgan("common"));
-app.use(bodyParser.json({limit: "30mb", extendedtrue }));
-app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
@@ -37,3 +38,19 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/*  ROUTES WITH FILES  */
+app.post("/auth/register", upload.single("picture"), register);
+
+
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
