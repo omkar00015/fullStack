87b92bbb42f4f745d18b342460f52cb9b0a46492
Omkar Paschapur <your.email@example.com>
2024-08-12 21:36:26 +0530

    authorization

---
+++ server/models/Post.js
@@ -0,0 +1,29 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+
+
+const postSchema = mongoose.Schema (
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        location : String,
+        description: String,
+        picturePath: String,
+        userPicturePath: String,
+        like: {
+            type: Map,
+            of: Boolean,
+        },
+        comments: {
+            types: Array,
+            default: [],
+        }
+    },
+    {    Timestamps: true}
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
