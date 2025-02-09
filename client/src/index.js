c7cf8769bda7abf37ef8ba7c4b4a4f3791e1974a
Omkar Paschapur <your.email@example.com>
2025-02-09 21:48:32 +0530

    front-end changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,41 +1,36 @@
-
-/// <reference types="redux-persist/types" /> 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.js';
-import authReducer from './state/index.js';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import {
+  persistStore,
+  persistReducer,
   FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
   PURGE,
   REGISTER,
-} from 'redux-persist';
-import { persistReducer, persistStore } from 'redux-persist';
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
-
-const storage = require('redux-persist/lib/storage');
-const { PersistGate } = require('redux-persist/integration/react');
-
-
-
-
-const persistConfig = { key: 'root', storage, version: 1 };
+const persistConfig = { key: "root", storage, version: 1 };
 const persistedReducer = persistReducer(persistConfig, authReducer);
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE],
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
     }),
 });
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- server/index.js
+++ server/index.js
@@ -57,15 +57,13 @@ app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
 
-    // /*  ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-})
-.catch((error) => console.log(`${error} did not connect`));
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+        // /*  ADD DATA ONE TIME */
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
